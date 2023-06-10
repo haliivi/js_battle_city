7a26af6fea7057250c84b84336bfa49d207d5e65
haliivi <haliivi@mail.ru>
2023-06-10 15:19:49 +0700

    1) Pass maineScene to instance of class Game. 2) Defined constructor class Scene.

--- engine/Scene.js
+++ engine/Scene.js
@@ -2,7 +2,23 @@
     'use strict'
 
     class Scene {
-        
+        constructor (args = {}) {
+            const {
+                autoStart=true,
+                loading,
+                init,
+                update} = args
+            this.autoStart = autoStart
+            this.loading = loading.bind(this)
+            this.init = init.bind(this)
+            this.update = update.bind(this)
+        }
+
+        loading () {}
+
+        init () {}
+
+        update () {}
     }
 
     window.GameEngine = window.GameEngine || {};
